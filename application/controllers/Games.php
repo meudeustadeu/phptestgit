bec444287bec9b8dc1b97fa51a5ba5ccbabfafcc
Tadeu Luiz <tadeu_2012xd@hotmail.com>
2022-03-25 17:08:12 -0300

    phptestgit

---
+++ application/controllers/Games.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Games extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->model("games_model");
+
+		$data['games'] = $this->games_model->index();
+		$data['title'] = 'Games - Codeigniter';
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/nav-top', $data);
+		$this->load->view('pages/games', $data);
+	}
+}
---
+++ application/models/Games_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class Games_model extends CI_Model{
+
+	public function index()
+	{
+		return $this->db->get("tb_games")->result_array();
+	}
+}
+?>
+
